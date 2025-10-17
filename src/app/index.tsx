1bff092cd0a9ffe8f3fab52644f4a090c35c683a
Diego <dpadula@epe.santafe.gov.ar>
2025-10-17 14:55:21 -0300

    usando pequeños estilos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,19 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Icon from '../../assets/images/wordle-icon.svg';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
+    <View style={styles.container}>
       <Text>Home</Text>
       <Icon width={100} height={100}></Icon>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
