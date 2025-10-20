0dd7fca37a15af27e15e6b5e8811c1a6e6ab40e2
Diego <dpadula@epe.santafe.gov.ar>
2025-10-20 07:55:08 -0300

    Layout del index groseramente

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,18 @@ import Icon from '../../assets/images/wordle-icon.svg';
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Icon width={100} height={100}></Icon>
+      <View style={styles.header}>
+        <Icon width={100} height={70}></Icon>
+        <Text style={styles.title}>Wordle</Text>
+        <Text style={styles.text}>Get 6 chances to guess a 5-letter word.</Text>
+      </View>
+      <View>
+        <Text style={styles.text}>Start a new game</Text>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.text}>Created by</Text>
+        <Text style={styles.text}>@simonwordle</Text>
+      </View>
     </View>
   );
 }
@@ -14,6 +24,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    paddingHorizontal: 30,
+  },
+  header: {
     alignItems: 'center',
+    gap: 10,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  footer: {
+    alignItems: 'center',
+    gap: 10,
   },
 });
